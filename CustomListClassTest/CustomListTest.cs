d15ba847b21ff3ac5ce1eec9673ad4fd5164f7eb
Victor D Verdev <dev@verdev.io>
2018-06-05 20:48:08 -0500

    Initial Commit

---
+++ CustomListClassTest/CustomListTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomListClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListClassTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CustomListClassTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListClassTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListClassTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b0a6b74-a649-4c2b-9dc1-47bb1531a6a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
